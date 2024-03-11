09acd48fcc5d5545f8717f31fa79e251dbbdc939
xm1ls <xm1ls2001@gmail.com>
2024-03-11 14:35:10 +0200

    Add Page components

--- app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_1/MainActivity.kt
@@ -3,13 +3,25 @@ package ua.edu.lntu.cw_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw_1.ui.theme.IPZ_CW_1_Hasyuk_VitalyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Page();
                 }
             }
         }
@@ -37,10 +49,90 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun Page(modifier: Modifier = Modifier) {
+    Column {
+        Profile(
+            image = R.drawable.ic_launcher_background,
+            "John Doe",
+            "Control work 1"
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Column() {
+            Info(
+                icon = R.drawable.baseline_local_phone_24,
+                info = "+11111111111"
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Info(
+                icon = R.drawable.baseline_share_24,
+                info = "@AndroidDev"
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Info(
+                icon = R.drawable.baseline_mail_24,
+                info = "mail@mail.com"
+            )
+
+        }
+    }
+}
+
+@Composable
+fun Profile(
+    image: Int,
+    name: String,
+    label: String,
+    modifier: Modifier = Modifier) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(
+                color = MaterialTheme.colorScheme.background,
+            )
+
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        )
+        Text(
+            text = "$name",
+        )
+        Text(
+            text = "$label",
+        )
+    }
+}
+
+@Composable
+fun Info(
+    icon: Int,
+    info: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+        )
+        Text(
+            text = "$info"
+        )
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IPZ_CW_1_Hasyuk_VitalyTheme {
-        Greeting("Android")
+        Page();
     }
 }
